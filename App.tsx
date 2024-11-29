535e33c2ec964517bd5be646e6e6ec373c67712d
Giuseppe Orlandi <giuseppe.po@hotmail.com>
2024-11-29 20:58:23 -0300

    Iniciando projeto.

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import { SafeAreaView, StyleSheet, Text } from "react-native"
+import Home from "./src/telas/Home"
+
+export default function App() {
+  return <SafeAreaView style={estilos.tela}>
+    <Home />
+  </SafeAreaView>
+}
+
+const estilos = StyleSheet.create({
+  tela: {
+    flex: 1
+  }
+})
---
+++ src/mocks/topo.js
@@ -0,0 +1,7 @@
+import logo from "../assets/logo.png"
+
+export const topo = {
+    imagem: logo,
+    boasVindas: "Olá Giuseppe",
+    legenda: "Encontre os melhores produtores"
+}
